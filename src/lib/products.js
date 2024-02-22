5fa846334d4695e97c51bbdde1a4f69173c75d61
arji30 <109784949+arji30@users.noreply.github.com>
2024-02-22 23:09:35 +0530

    final commit

--- src/lib/products.js
+++ src/lib/products.js
@@ -1,1 +1,16 @@
 //test products data
+const products = [
+    {
+      id: "1",
+      name: "Product 1",
+      image: "product1.jpg",
+    },
+    {
+      id: "2",
+      name: "Product 2",
+      image: "product2.jpg",
+    },
+  
+  ];
+
+  export default products;
