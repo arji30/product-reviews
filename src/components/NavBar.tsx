c75e1821387edfe4275fed3825996026136e9f91
arji30 <109784949+arji30@users.noreply.github.com>
2024-02-22 22:10:57 +0530

    fixing build errors

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { signOut } from "next-auth/react";
 import { hostUrl } from "@/lib/variables";
+import Link from 'next/link';
 
 interface NavbarProps {
     username: string | undefined;
